00f39ec2d1efab6dce01251d6f21bdd3886cd478
na5im <72241606+na5im@users.noreply.github.com>
2022-10-17 22:07:46 -0400

    added top obstacle + pipe image

--- client/flappy_bird/app.js
+++ client/flappy_bird/app.js
@@ -3,17 +3,18 @@ document.addEventListener('DOMContentLoaded', () => {
   const gameDisplay = document.querySelector('.game-container');
   const ground = document.querySelector('.ground');
 
-  let birdLeft = 220;
+  const birdLeft = 220;
   let birdBottom = 100;
-  let gravity = 2;
+  const gravity = 2;
   let isGameOver = false;
+  const gap = 430;
 
   function startGame() {
     birdBottom -= gravity;
     bird.style.bottom = `${birdBottom}px`;
     bird.style.left = `${birdLeft}px`;
   }
-  let gameTimerId = setInterval(startGame, 20);
+  const gameTimerId = setInterval(startGame, 20);
 
   function jump() {
     if (birdBottom < 500) birdBottom += 50;
@@ -30,27 +31,37 @@ document.addEventListener('DOMContentLoaded', () => {
 
   function generateObstacle() {
     let obstacleLeft = 500;
-    let randomHeight = Math.random() * 60;
-    let obstacleBottom = randomHeight;
+    const randomHeight = Math.random() * 60;
+    const obstacleBottom = randomHeight;
     const obstacle = document.createElement('div');
+    const topObstacle = document.createElement('div');
 
-    if (!isGameOver) obstacle.classList.add('obstacle');
+    if (!isGameOver) {
+      obstacle.classList.add('obstacle');
+      topObstacle.classList.add('topObstacle');
+    }
     gameDisplay.appendChild(obstacle);
+    gameDisplay.appendChild(topObstacle);
+
     obstacle.style.left = `${obstacleLeft}px`;
+    topObstacle.style.left = `${obstacleLeft}px`;
     obstacle.style.bottom = `${obstacleBottom}px`;
+    topObstacle.style.bottom = `${obstacleBottom + gap}px`;
 
     function moveObstacle() {
       obstacleLeft -= 2;
       obstacle.style.left = `${obstacleLeft}px`;
+      topObstacle.style.left = `${obstacleLeft}px`;
 
       if (obstacleLeft === -60) {
         clearInterval(timerId);
         gameDisplay.removeChild(obstacle);
+        gameDisplay.removeChild(topObstacle);
       }
       if (
-        obstacleLeft > 200 && obstacleLeft < 280 && birdLeft === 220 &&
-        birdBottom  < obstacleBottom + 153 ||
-        birdBottom === 0
+        obstacleLeft > 200 && obstacleLeft < 280 && birdLeft === 220
+        && (birdBottom < obstacleBottom + 153 || birdBottom > obstacleBottom + gap - 200)
+        || birdBottom === 0
       ) {
         gameOver();
         clearInterval(timerId);
