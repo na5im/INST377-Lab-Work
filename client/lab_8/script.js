fa28090439ae09917e05caa3a88c3f38c767e2e1
na5im <72241606+na5im@users.noreply.github.com>
2022-11-17 19:13:40 -0500

    added map

--- client/lab_8/script.js
+++ client/lab_8/script.js
@@ -94,6 +94,15 @@ function filterList(list, filterInputValue) {
   });
 }
 
+function initMap() {
+  console.log('initMap');
+  const map = L.map('map').setView([38.9849, -76.9378], 13);
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+}
+
 async function mainEvent() {
   /*
     ## Main Event
@@ -101,7 +110,7 @@ async function mainEvent() {
       When you're not working in a heavily-commented "learning" file, this also is more legible
       If you separate your work, when one piece is complete, you can save it and trust it
   */
-
+  initMap();
   // the async keyword means we can make API requests
   const form = document.querySelector('.main_form'); // get your main form so you can do JS with it
   const submit = document.querySelector('#get-resto'); // get a reference to your submit button
